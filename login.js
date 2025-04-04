fce2c5c4068f0da2700391b5d482e77ab01e604e
jeonghwan <jxxnghwxn@gmail.com>
2025-04-04 22:50:46 +0900

    login/html, css, js

---
+++ login.js
@@ -0,0 +1,12 @@
+function popId() {
+  const userID = document.getElementById('txt_id').value;
+  if (!userID) {
+    alert('아이디 입력');
+  } else {
+    alert(userID);
+  }
+}
+
+function myFunction() {
+  alert('clicked');
+}
